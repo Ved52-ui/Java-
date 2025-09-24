d6e69069bb5084ddd20df3959edc722cf896907f
Ved Patel <137433618+Ved52-ui@users.noreply.github.com>
2025-09-24 10:25:44 +0530

    Add files via upload
    java files

---
+++ my.java
@@ -0,0 +1,7 @@
+public class my {
+    public static void main(String[] args) {
+        String a = "the quick brown fox is jumps over the lazy dog";
+        int[] num = {50,100,150,200,250};
+        System.out.println(num);
+    }
+}     
